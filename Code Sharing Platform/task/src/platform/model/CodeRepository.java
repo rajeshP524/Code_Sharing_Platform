af3f3957ae14350de357806fc7d689acc02427bc
rajeshP524 <rajeshpulapakula24@gmail.com>
2021-07-27 20:08:21 +0530

    Code Repository Interface

---
+++ Code Sharing Platform/task/src/platform/model/CodeRepository.java
@@ -0,0 +1,10 @@
+package platform.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CodeRepository extends CrudRepository<Code, String> {
+
+}
